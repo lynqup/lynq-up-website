ab4841211adec6c50c216c324965d0b08cce1884
Lynq Up <info@lynqup.net>
2026-01-07 09:42:16 +0100

    add allowed hosts2

--- studybud/settings.py
+++ studybud/settings.py
@@ -25,13 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '.railway.app',
-    'lynqup.nl',
-    'www.lynqup.nl',
-]
+
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
 CSRF_TRUSTED_ORIGINS = [
