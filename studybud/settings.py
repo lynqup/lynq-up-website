bce2e7a0eb234eaaa4704028864c475584693828
Lynq Up <info@lynqup.net>
2026-01-07 09:39:17 +0100

    add allowed hosts2

--- studybud/settings.py
+++ studybud/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&m*+u1pct83e+%+h8c+n2k^o$&e#5jtt$d^hun+r)%=o_0iebt'
+#SECRET_KEY = 'django-insecure-&m*+u1pct83e+%+h8c+n2k^o$&e#5jtt$d^hun+r)%=o_0iebt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
